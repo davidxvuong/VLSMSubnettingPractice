21c587e9cfa4efcb66be23d17c402b643baae3bd
David Vuong <david.vuong@uwaterloo.ca>
2015-07-23 22:49:18 -0400

    Created UI

--- VLSMSubnettingPracticeUI/VLSMSubnettingPracticeUI/MainWindow.xaml.cs
+++ VLSMSubnettingPracticeUI/VLSMSubnettingPracticeUI/MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace VLSMSubnettingPracticeUI
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("VLSM Subnetting Practice\nDeveloped by David Vuong\nhttp://davidvuong.ca\n\nLEGAL INFORMATION\n\nTHE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.", "About", MessageBoxButton.OK);
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            lblQuestion.Text += "HELLO";
+        }
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
